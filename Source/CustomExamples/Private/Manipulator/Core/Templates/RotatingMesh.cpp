e2340f0166dfac8b99772796f541a1cc7e4a397e
Igor Vasilev <Igor.Vasiliev@waveaccess.ru>
2022-11-01 16:13:10 +0300

    RotatingMesh: add timeline functionality;

--- Source/CustomExamples/Private/Manipulator/Core/Templates/RotatingMesh.cpp
+++ Source/CustomExamples/Private/Manipulator/Core/Templates/RotatingMesh.cpp
@@ -3,7 +3,70 @@
 
 #include "Manipulator/Core/Templates/RotatingMesh.h"
 
-URotatingMeshComponent::~URotatingMeshComponent()
-{
 
+URotatingMeshComponent::URotatingMeshComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	InitConstruction();
+}
+
+void URotatingMeshComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+void URotatingMeshComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	Timeline.TickTimeline(DeltaTime);
+}
+
+void URotatingMeshComponent::InitConstruction()
+{
+	InitTimeline();
+}
+
+void URotatingMeshComponent::InitTimeline()
+{	
+	// Set the anim curve by default (just for example). actually do it by an inherited bp
+	FString CurveDir = "/CustomExamples/Manipulator/Resources/Objects/RoboParts/Curve_Robo.Curve_Robo";
+	static ConstructorHelpers::FObjectFinder<UCurveFloat> Curve(*CurveDir);
+	if (Curve.Succeeded())
+	{
+		AnimCurve = Curve.Object;
+	}
+	
+	Timeline.SetPlayRate(AnimRate);
+
+	FOnTimelineFloat UpdatingFunction{};
+	UpdatingFunction.BindUFunction(this, "Rotating");
+	Timeline.AddInterpFloat(AnimCurve, UpdatingFunction, FName{ TEXT("Curve_Robo") });
+	
+	FOnTimelineEventStatic OnTimelineFinished{};
+	OnTimelineFinished.BindUObject(this, &URotatingMeshComponent::RotatingFinished);
+	Timeline.SetTimelineFinishedFunc(OnTimelineFinished);
+}
+
+void URotatingMeshComponent::Rotate(const FRotator& InTargetRotation)
+{
+	InitialRotation = GetRelativeRotation();
+	TargetRotation = InTargetRotation;
+
+	Timeline.PlayFromStart();
+}
+
+void URotatingMeshComponent::Rotating(const float Value)
+{
+	FRotator NewRotation = FMath::Lerp(InitialRotation, TargetRotation, Value);
+	SetRelativeRotation(NewRotation);
+}
+
+void URotatingMeshComponent::RotatingFinished()
+{
+	OnRotationFinished.ExecuteIfBound();
+
+	//test
+	UE_LOG(LogCustomExample, Warning, TEXT("%s -- RotatingFinished."), *FString(__FUNCTION__));
 }
--- Source/CustomExamples/Public/Manipulator/Core/Templates/RotatingMesh.h
+++ Source/CustomExamples/Public/Manipulator/Core/Templates/RotatingMesh.h
@@ -2,18 +2,46 @@
 
 #pragma once
 
+#include "Components/TimelineComponent.h"
 #include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
 #include "RotatingMesh.generated.h"
 
-/**
- * 
- */
+DECLARE_DELEGATE(FOnRotationFinished);
+
+/** Mesh component with rotation animation. */
 UCLASS(Blueprintable)
 class CUSTOMEXAMPLES_API URotatingMeshComponent : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
 public:
-	~URotatingMeshComponent();
+	URotatingMeshComponent();
+	
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction);
+	
+	UFUNCTION(BlueprintCallable, Category = "Manipulator|RotatingMesh")
+	void Rotate(const FRotator& InTargetRotation);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Manipulator|RotatingMesh")
+	float AnimRate = 1.0;
+
+	FOnRotationFinished OnRotationFinished;
+
+private:
+	void InitConstruction();
+	void InitTimeline();
+
+	UFUNCTION()
+	void Rotating(const float Value);
+	void RotatingFinished();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Manipulator|RotatingMesh", meta = (AllowPrivateAccess = "true"))
+	UCurveFloat* AnimCurve;
+
+	FTimeline Timeline; 
+
+	FRotator InitialRotation;
+	FRotator TargetRotation;
 };
